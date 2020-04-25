b5b26c8a032cddb764b5b0c665353722af72717f
xchen1 <xchen1@tcl.com>
2020-04-25 12:05:12 +0800

    add tomcat

--- a2-test/src/main/java/com/SpringBoot内嵌Tomcat.java
+++ a2-test/src/main/java/com/SpringBoot内嵌Tomcat.java
@@ -1,17 +1,10 @@
 package com;
 
 
-import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
-import org.apache.catalina.WebResourceRoot;
-import org.apache.catalina.core.AprLifecycleListener;
-import org.apache.catalina.core.StandardContext;
-import org.apache.catalina.core.StandardServer;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.webresources.DirResourceSet;
-import org.apache.catalina.webresources.StandardRoot;
 
-import java.io.File;
+import javax.servlet.ServletException;
 
 /**
  * 1、添加tomcat 依赖
@@ -23,59 +16,46 @@ import java.io.File;
  */
 public class SpringBoot内嵌Tomcat {
 
+	public static int TOMCAT_PORT = 8080;
+	public static String TOMCAT_HOSTNAME = "127.0.0.1";
+	public static String WEBAPP_PATH = "a2-test/web";
 
 
-public static void 	run() throws LifecycleException {
-	Tomcat tomcat = new Tomcat();
-	tomcat.setPort(9090);
-
-	// 设置contextPath和路径
-	String docBase = new File("a2-test/webapp").getAbsolutePath();
-
-	StandardContext ctx = (StandardContext) tomcat.addWebapp("/",docBase);
-	ctx.setReloadable(false);
-	File additionWebInfClasses = new File("target/classes");
-	WebResourceRoot resources = new StandardRoot(ctx);
-	resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClasses.getAbsolutePath(),
-			"/"));
-	tomcat.start();
-	tomcat.getServer().await();
+	public static void run() throws ServletException, LifecycleException {
+		Tomcat tomcat = new Tomcat();
+		tomcat.setPort(SpringBoot内嵌Tomcat.TOMCAT_PORT);
+		/*
+		 * https://www.cnblogs.com/ChenD/p/10061008.html
+		 */
+		tomcat.addWebapp("/",  "/local/sda/project/spring-framework-master/a2-test/web");
+//		/*
+//		 * true时：相关classes | jar 修改时，会重新加载资源，不过资源消耗很大
+//		 * autoDeploy 与这个很相似，tomcat自带的热部署不是特别可靠，效率也不高。生产环境不建议开启。
+//		 * 相关文档：
+//		 * http://www.blogjava.net/wangxinsh55/archive/2011/05/31/351449.html
+//		 */
+//		myCtx.setReloadable(false);
+//		// 上下文监听器
+//		myCtx.addLifecycleListener(new AprLifecycleListener());
+//
+//        /*String webAppMount = System.getProperty("user.dir") + File.separator + TomcatStart.CLASS_PATH;
+//        WebResourceRoot root = new StandardRoot(myCtx);
+//        root.addPreResources(
+//            new DirResourceSet(root, TomcatStart.WEBINF_CLASSES, webAppMount, TomcatStart.INTERNAL_PATH));*/
+//
+//		// 注册servlet
+//		tomcat.addServlet("/access", "demoServlet", new T());
+//		// servlet mapping
+//		myCtx.addServletMappingDecoded("/demo.do", "demoServlet");
+		tomcat.start();
+		tomcat.getServer().await();
 	}
 
 
 
-	public static void main(String[] args) throws LifecycleException {
+	public static void main(String[] args) throws LifecycleException, ServletException {
 
 		SpringBoot内嵌Tomcat.run();
-//		long start = System.currentTimeMillis();
-//		// 设置端口
-//		int port = 8888;
-//		Tomcat tomcat = new Tomcat();
-//		tomcat.setPort(port);
-//
-//		// 添加listener
-//		StandardServer server = (StandardServer) tomcat.getServer();
-//		AprLifecycleListener listener = new AprLifecycleListener();
-//		server.addLifecycleListener(listener);
-//
-//		// 设置contextPath和路径
-//		String contextPath = "/a2-test";
-//		String docBase = new File("webapp").getAbsolutePath();
-//		Context context = tomcat.addWebapp(contextPath, docBase);
-//		System.out.println("添加contextPath和docBase是:" + contextPath + "==>" + docBase);
-//
-//		// add servlet
-//		// Context context = tomcat.addContext(contextPath, baseDir);
-//		String servletName = "hello";
-//		String servletMapping = "/hello";
-//		tomcat.addServlet(contextPath, servletName, new HelloServlet());
-//		context.addServletMappingDecoded(servletMapping, servletName);
-//		// 启动tomcat
-//		tomcat.start();
-//		long end = System.currentTimeMillis();
-//		System.out.println("启动完成,共使用了:" + (end - start) + "ms");
-//		// 进入监听状态,如果不进入监听状态,启动tomat后就会关闭tomcat
-//		tomcat.getServer().await();
 	}
 
 }
